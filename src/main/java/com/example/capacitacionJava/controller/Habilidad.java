f36437001bbdd1713ac0cc337096dd184d66bfc0
Faber Aguirre <faberaj89@gmail.com.com>
2023-09-16 11:47:35 -0500

    first commit

---
+++ src/main/java/com/example/capacitacionJava/controller/Habilidad.java
@@ -0,0 +1,16 @@
+package com.example.capacitacionJava;
+
+public class Habilidad {
+    private String nombre;
+    private String nivel;
+    private String experiencia;
+    private String descripcion;
+
+    // Constructor
+    public Habilidad(String nombre, String nivel, String experiencia, String descripcion) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.experiencia = experiencia;
+        this.descripcion = descripcion;
+    }
+}
