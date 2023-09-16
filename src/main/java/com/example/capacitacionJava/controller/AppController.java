f44dd66f12459bc2dfc60de059c11b15adeeb162
Faber Aguirre <faberaj89@gmail.com.com>
2023-09-16 11:50:26 -0500

    commit

--- src/main/java/com/example/capacitacionJava/controller/AppController.java
+++ src/main/java/com/example/capacitacionJava/controller/AppController.java
@@ -3,22 +3,46 @@ package com.example.capacitacionJava.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class AppController {
 
-    @GetMapping("/")
-    public String home(){
-        return "login";
-    }
-    @GetMapping("/login")
-    public String login(){
-        return "login";
+
+    @GetMapping("/contacto")
+    public String mostrarPaginaContacto() {
+        return "contacto";
     }
 
-    @GetMapping("/createAccount")
-    public String createAccount(){
-        return "createAccount";
+    @GetMapping("/proyectos")
+    public String mostrarHabilidades(Model model) {
+        List<Habilidad> habilidades = obtenerHabilidades();
+        model.addAttribute("habilidades", habilidades);
+
+        return "proyectos";
+    }
+
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        List<Habilidad> habilidades = obtenerHabilidades();
+        model.addAttribute("habilidades", habilidades);
+        return "index";
+    }
+
+
+    private List<Habilidad> obtenerHabilidades() {
+        List<Habilidad> habilidades = new ArrayList<>();
+
+        habilidades.add(new Habilidad("Programación Java", "Avanzado", "5 años", "Desarrollo de aplicaciones Java."));
+        habilidades.add(new Habilidad("HTML/CSS", "Intermedio", "3 años", "Diseño y desarrollo web."));
+        habilidades.add(new Habilidad("JavaScript", "Intermedio", "2 años", "Desarrollo de scripts y aplicaciones web interactivas."));
+        habilidades.add(new Habilidad("SqlServer", "Avanzado", "2 años", "Desarrollo de scripts y optimizacion de consultas"));
+        habilidades.add(new Habilidad("Power BI", "Avanzado", "2 años", "Desarrollo de tableros interactivos"));
+
+        return habilidades;
     }
 
 }
--- src/main/java/com/example/capacitacionJava/controller/Habilidad.java
+++ src/main/java/com/example/capacitacionJava/controller/Habilidad.java
@@ -1,4 +1,4 @@
-package com.example.capacitacionJava;
+package com.example.capacitacionJava.controller;
 
 public class Habilidad {
     private String nombre;
@@ -11,6 +11,38 @@ public class Habilidad {
         this.nombre = nombre;
         this.nivel = nivel;
         this.experiencia = experiencia;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(String experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
 }
